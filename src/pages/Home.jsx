c5a0de9bf1969b77e5eb029f95aa448818cc6519
mxebada <ebadamohamed984@gmail.com>
2024-04-26 04:47:10 +0300

    yy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import Navbar from "../components/Navbar/Navbar";
-import MyModal from "../components/MyModal";
-import MyPay from "../components/MyPay";
 const Home = () => {
   return (
-    <div style={{backgroundColor:"red",height:"2000px"}}>
-      <Navbar/>
-      {/* <MyModal /> */}
-      <MyPay/>
+    <div>
+      Home
     </div>
   );
 };
