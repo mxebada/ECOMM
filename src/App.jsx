6f9539ed2649435eabca63473f2b5a239298b0e4
mxebada <ebadamohamed984@gmail.com>
2024-04-26 04:44:47 +0300

    Revert "jj"
    This reverts commit 8dbcad2c8bd36eb8fb502e7b80117fbfe25e2b80.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-import React, { useEffect } from "react";
+import React from "react";
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import translationAr from "./locale/ar/translation.json"
-import translationEn from "./locale/en/translation.json"
-// import HttpApi from 'i18next-http-backend';
-import Cookies from "js-cookie";
+// import translationAr from "./locale/ar/translation.json"
+// import translationEn from "./locale/en/translation.json"
+import HttpApi from 'i18next-http-backend';
+
+i18next.use(HttpApi).init(i18nextOptions);
 
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
-  // .use(HttpApi)
+  .use(HttpApi)
   .init({
-    resources: {
-      en: {
-        translation: translationEn
-      },
-      ar: {
-        translation: translationAr
-      },
-    },
+    // resources: {
+    //   en: {
+    //     translation: translationEn
+    //   },
+    //   ar: {
+    //     translation: translationAr
+    //   },
+    // },
     fallbackLng: "en",
     detection: {
       order: [
@@ -35,33 +36,16 @@ i18n
       ],
       caches: ["cookie"],
     },
-    // backend: {
-    //   loadPath: "./locale/{{lng}}/translation.json",
-    // },
+    backend: {
+      loadPath: '/locale/{{lng}}/translation.json',
+    },
   });
 
 const App = () => {
   const { t } = useTranslation();
-
-  const lng = "en" || Cookies.get("i18next");
-
-  useEffect(() => {
-    window.document.dir = i18n.dir();
-    setTimeout(() => {
-      setLoading(false);
-    }, 500);
-  }, [lng]);
-
-
   return (
     <div>
       <h2>{t("Welcome to React")}</h2>
-      <button onClick={()=> {
-        i18n.changeLanguage("ar")
-      }}>Ar</button>
-      <button onClick={()=> {
-        i18n.changeLanguage("en")
-      }}>En</button>
     </div>
   );
 };
